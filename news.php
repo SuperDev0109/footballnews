f58ee61df5893f40de9003cf2b5868569ca66698
super-dev008 <super008008@outlook.com>
2022-03-08 15:18:00 -0800

    google analytics addes

--- news.php
+++ news.php
@@ -6,6 +6,8 @@
 	<title>Football</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/news.css" />
+    <script async src=“https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5224574695975714”
+     crossorigin=“anonymous”></script>
 </head>
 <body class="homepage" style="font-family: monospace;">
 
