9af8e2d4d24b0ed6811356113694cd4d3d53690e
superdev0109 <superdev0109@outlook.com>
2022-04-04 16:40:33 -0700

    detail.js added

--- js/detail.js
+++ js/detail.js
@@ -6,7 +6,7 @@ $(document).ready( function(){
             story: $("#storyValue").val()
         },
         success: function (html, status, xhr) {
-            $('.news_body .panel2').each(function(index) {
+            $('.news_body_detail .panel2').each(function(index) {
                 $(this).children('.title1').html( JSON.parse(html).title );
                 $(this).children('.title_detail').html( JSON.parse(html).desc );
                 $(this).children('.title_alt').html( JSON.parse(html).alt );
